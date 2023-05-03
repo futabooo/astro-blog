a2d2de9ad0f3fc3087805377f20e71ea02b8a0ca
futabooo <takahiro.futagawa@gmail.com>
2023-05-04 00:33:55 +0900

    add tailwind eslint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
 import cloudflare from "@astrojs/cloudflare";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
