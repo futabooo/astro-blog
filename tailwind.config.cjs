4622a1ff0b0d1878221f66838721f4832ad12c2a
futabooo <takahiro.futagawa@gmail.com>
2023-05-07 01:45:39 +0900

    update design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,6 @@ module.exports = {
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   // daisyUI config (optional)
   daisyui: {
-    themes: ["business", "fantasy"],
+    themes: ["night", "fantasy"],
   },
 };
