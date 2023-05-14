892b931d9e0eeb31c436b53ea501ba9578fc4a1d
futabooo <takahiro.futagawa@gmail.com>
2023-05-14 21:58:47 +0900

    add social icon

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,8 @@
 export const SITE_AUTHOR = "futabooo";
 export const SITE_TITLE = "futabooo";
 export const SITE_DESCRIPTION = "futabooo's personal site";
+
+// image
+export const IC_FUTABOOO = "ic_futabooo_orenge.jpg"
+export const IC_GITHUB = "ic_github.svg"
+export const IC_TWITTER = "ic_twitter.svg"
